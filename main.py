e1f4ff5671bec6a6eb6051a840ab636cb3d3d980
Steshin <@gs.pmok>
2022-06-28 16:52:06 +0300

    Models done

--- main.py
+++ main.py
@@ -1,29 +1,27 @@
-import os
-import configparser
-import sqlalchemy as sq
-from sqlalchemy.orm import declarative_base, relationship, sessionmaker
 
 
-class Connect:
-    def __init__(self):
-        self.session = None
-        self.engine = None
-        self.DSN = None
-        self.config = 'config.ini'
 
-    def get_dsn(self):
-        config = configparser.ConfigParser()
-        config.read(self.config)
-        system = config["Postgres"]["system"]
-        name = config["Postgres"]["name"]
-        password = config["Postgres"]["password"]
-        db_name = config["Postgres"]["db_name"]
-        self.DSN = f"{system}://{name}:{password}@localhost:5432/{db_name}"
+def get_dsn():
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+    system = config["Postgres"]["system"]
+    name = config["Postgres"]["name"]
+    password = config["Postgres"]["password"]
+    db_name = config["Postgres"]["db_name"]
+    DSN = f"{system}://{name}:{password}@localhost:5432/{db_name}"
+    return DSN
 
-    def get_connection(self):
-        self.engine = sq.create_engine(self.DSN)
-        Session = sessionmaker(bind=self.engine)
-        self.session = Session()
 
-    def end(self):
-        self.session.close()
+def get_engine(dsn):
+    engine = sq.create_engine(dsn)
+
+
+def get_session(engine):
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+
+def read_json(data):
+    with open(data, encoding='UTF-8') as file:
+        work = json.load(file)
+        return work
