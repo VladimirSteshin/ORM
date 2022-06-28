a1add49fdee8b646350d9c84372a2c0f1f07a543
Steshin <@gs.pmok>
2022-06-28 19:48:57 +0300

    Base filled, write query, requirements and go home

--- data.py
+++ data.py
@@ -1,7 +1,5 @@
-import json
-import configparser
 import sqlalchemy as sq
-from sqlalchemy.orm import declarative_base, relationship, sessionmaker
+from sqlalchemy.orm import declarative_base, relationship
 
 Base = declarative_base()
 
@@ -15,15 +13,15 @@ class Publisher(Base):
 class Book(Base):
     __tablename__ = 'book'
     id = sq.Column(sq.Integer, primary_key=True)
-    title = sq.Column(sq.String(length=40), nullable=False)
-    publisher_id = sq.Column(sq.Integer, sq.ForeignKey('publisher.id'), nullable=False)
+    title = sq.Column(sq.String(length=80), nullable=False)
+    id_publisher = sq.Column(sq.Integer, sq.ForeignKey('publisher.id'), nullable=False)
     publisher = relationship(Publisher, backref='book')
 
 
 class Shop(Base):
     __tablename__ = 'shop'
     id = sq.Column(sq.Integer, primary_key=True)
-    name = sq.Column(sq.String, nullable=False)
+    name = sq.Column(sq.String(length=40), unique=True)
 
 
 class Stock(Base):
@@ -31,39 +29,21 @@ class Stock(Base):
     id = sq.Column(sq.Integer, primary_key=True)
     id_shop = sq.Column(sq.Integer, sq.ForeignKey('shop.id'), nullable=False)
     id_book = sq.Column(sq.Integer, sq.ForeignKey('book.id'), nullable=False)
-    count = sq.Column(sq.Integer, )
+    count = sq.Column(sq.Integer, nullable=False, default=0)
     shop = relationship(Shop, backref='stock')
-    book = relationship(Book, backref='book')
+    book = relationship(Book, backref='stock')
 
 
 class Sale(Base):
     __tablename__ = 'sale'
     id = sq.Column(sq.Integer, primary_key=True)
-    price = sq.Column(sq.Integer, nullable=False)
+    price = sq.Column(sq.String, nullable=False)
     date_sale = sq.Column(sq.TIMESTAMP, nullable=False)
     count = sq.Column(sq.Integer, nullable=False)
-    id_stock = sq.Column(sq.Integer, sq.ForeignKey('stock.id'), nullable=False)
+    id_stock = sq.Column(sq.Integer, sq.ForeignKey('stock.id'))
+    stock = relationship(Stock, backref='sale')
 
 
 def create_tables(engine):
     Base.metadata.drop_all(engine)
     Base.metadata.create_all(engine)
-
-
-def get_dsn():
-    config = configparser.ConfigParser()
-    config.read('config.ini')
-    system = config["Postgres"]["system"]
-    name = config["Postgres"]["name"]
-    password = config["Postgres"]["password"]
-    db_name = config["Postgres"]["db_name"]
-    DSN = f"{system}://{name}:{password}@localhost:5432/{db_name}"
-    return DSN
-
-
-def get_engine(dsn):
-    engine = sq.create_engine(dsn)
-    create_tables(engine)
-
-
-get_engine(get_dsn())
--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-
+import json
+import configparser
+import sqlalchemy as sq
+from sqlalchemy.orm import sessionmaker
+from data import create_tables, Publisher, Book, Shop, Stock, Sale
 
 
 def get_dsn():
@@ -8,20 +12,47 @@ def get_dsn():
     name = config["Postgres"]["name"]
     password = config["Postgres"]["password"]
     db_name = config["Postgres"]["db_name"]
-    DSN = f"{system}://{name}:{password}@localhost:5432/{db_name}"
+    host = config["Postgres"]["host"]
+    port = config["Postgres"]["port"]
+    DSN = f"{system}://{name}:{password}@{host}:{port}/{db_name}"
     return DSN
 
 
-def get_engine(dsn):
+def get_engine():
+    dsn = get_dsn()
     engine = sq.create_engine(dsn)
+    return engine
 
 
-def get_session(engine):
+def fill_db():
+    engine = get_engine()
     Session = sessionmaker(bind=engine)
     session = Session()
-
-
-def read_json(data):
-    with open(data, encoding='UTF-8') as file:
+    with open('test_data.json', encoding='UTF-8') as file:
         work = json.load(file)
-        return work
+        for data in work:
+            model = {
+                'publisher': Publisher,
+                'book': Book,
+                'shop': Shop,
+                'stock': Stock,
+                'sale': Sale
+            }[data.get('model')]
+            session.add(model(id=data.get('pk'), **data.get('fields')))
+        session.commit()
+    session.close()
+
+
+def find_publisher(publisher):
+    engine = get_engine()
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    if str(publisher).isdigit():
+        q = session.query(Book).join(Stock.book).filter(Book.id_publisher == publisher)
+        for book in q.all():
+            print(book.id_publisher)
+
+
+create_tables(get_engine())
+fill_db()
+find_publisher(1)
